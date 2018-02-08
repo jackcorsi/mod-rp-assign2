8bfd92f9d0cae2eb33f95bdbe67e4767d4cccc73
jxc1090 <jxc1090@student.bham.ac.uk>
2018-02-08 14:09:10 +0000

    Started attempt at proportional feedback follower, still doesn't work

---
+++ src/rp/assignments/group/assm2/Part1_1.java
@@ -0,0 +1,30 @@
+package rp.assignments.group.assm2;
+
+import lejos.nxt.Button;
+import lejos.nxt.SensorPort;
+import lejos.nxt.UltrasonicSensor;
+import lejos.robotics.navigation.DifferentialPilot;
+import rp.config.RobotConfigs;
+import rp.config.WheeledRobotConfiguration;
+import rp.systems.WheeledRobotSystem;
+
+public class Part1_1 {
+	
+	private static final int TARGET_DISTANCE = 70;
+	private static final WheeledRobotConfiguration TAYYAB_CONFIG = RobotConfigs.EXPRESS_BOT;
+	private static final SensorPort sensorPort = SensorPort.S2;
+
+	public static void main(String[] args) {
+		System.out.println("Assignment 2 part 1 of part 1");
+		System.out.println("Press any button");
+		Button.waitForAnyPress();
+		DifferentialPilot pilot = (new WheeledRobotSystem(TAYYAB_CONFIG)).getPilot();
+		UltrasonicSensor sensor = new UltrasonicSensor(sensorPort);
+		ProportionalFeedbackController controller = new ProportionalFeedbackController(pilot, sensor, TARGET_DISTANCE);
+		controller.setDaemon(true); //Just in case it goes rogue and doesn't die for some reason
+		controller.start();
+		Button.waitForAnyPress();
+		controller.stop();
+	}
+
+}
---
+++ src/rp/assignments/group/assm2/ProportionalFeedbackController.java
@@ -0,0 +1,48 @@
+package rp.assignments.group.assm2;
+
+import lejos.nxt.UltrasonicSensor;
+import lejos.robotics.navigation.DifferentialPilot;
+import rp.util.Rate;
+
+public class ProportionalFeedbackController extends Thread {
+	
+	private static final int SLEEP_DURATION = 2;
+	private static final float SPEED_MULTIPLIER = 100.0f;
+	
+	private DifferentialPilot pilot;
+	private UltrasonicSensor sensor;
+	private int targetDistance;
+	private boolean running;
+	
+	public ProportionalFeedbackController(DifferentialPilot pilot, UltrasonicSensor sensor, int targetDistance) {
+		this.pilot = pilot;
+		this.sensor = sensor;
+		this.targetDistance = targetDistance;
+	}
+	
+	public void stop() {
+		running = false;
+	}
+	
+	@Override
+	public void run() {
+		running = true;
+		Rate rate = new Rate(SLEEP_DURATION);
+		pilot.setTravelSpeed(10.0);
+		pilot.forward();
+		
+		while (running && !isInterrupted()) {
+			double speed = (sensor.getDistance() - targetDistance) * SPEED_MULTIPLIER;
+			if (speed < 0)
+				speed = 0;
+			else if (speed > pilot.getMaxTravelSpeed())
+				speed = pilot.getMaxTravelSpeed();
+			
+			pilot.setTravelSpeed(speed);
+			
+			System.out.println(pilot.getTravelSpeed());
+			rate.sleep();
+		}
+		pilot.stop();
+	}
+}
